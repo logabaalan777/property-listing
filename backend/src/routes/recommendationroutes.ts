1d067a54c497199fa9ea6522ad54679958ad7867
logabaalan <115532100+logabaalan777@users.noreply.github.com>
2025-05-30 18:35:54 +0530

    Update recommendationroutes

--- backend/src/routes/recommendationroutes.ts
+++ backend/src/routes/recommendationroutes.ts
@@ -4,10 +4,8 @@ import { protect } from '../middleware/authmiddleware';
 
 const router = express.Router();
 
-// Recommend property to another user (by email)
 router.post('/', protect, recommendProperty);
 
-// View all recommendations received by logged-in user
 router.get('/received', protect, getReceivedRecommendations);
 
 router.delete('/:id', protect, deleteRecommendation);
