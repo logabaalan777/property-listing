eff6cdbf7682f2cb0880c5daa72b991a3a36e416
logabaalan <115532100+logabaalan777@users.noreply.github.com>
2025-05-30 18:35:19 +0530

    Update propertyroutes

--- backend/src/routes/propertyroutes.ts
+++ backend/src/routes/propertyroutes.ts
@@ -11,21 +11,16 @@ import { protect } from '../middleware/authmiddleware';
 
 const router = express.Router();
 
-// Public: get list of properties with filters
 router.get('/', getProperties);    
 
 router.get('/user', protect, getPropertiesByUser);
 
-// Public: get one property by id
 router.get('/:id', getPropertyById);
 
-// Protected: create a property (user must be logged in)
 router.post('/', protect, createProperty);
 
-// Protected: update a property (only owner)
 router.put('/:id', protect, updateProperty);
 
-// Protected: delete a property (only owner)
 router.delete('/:id', protect, deleteProperty);
 
 export default router;
